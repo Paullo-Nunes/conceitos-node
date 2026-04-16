4a157d510d3cc6eaa5a87ee4b6b85527967ae74f
Dev Js <alienwaredev369@outlook.com>
2026-04-16 06:15:18 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+app.use(express.json())
+
+const users = [] //para salvar os usuarios, mas não é profissional porque quando a página é atualizada os dados somem
+
+//rota de listar, buscar e obter
+
+app.get('/usuario', (req, res) => {
+    
+    res.status(200).json(users)
+})
+
+//rota de criar
+
+app.post('/usuario', (req, res) => {
+    users.push(req.body) //enviando a requisição para o arry users
+    res.status(201).json({message: "Usuário criado com sucesso"})
+})
+
+app.listen(3000)
