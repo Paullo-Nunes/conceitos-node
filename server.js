1dea76c2cd73ab95a5b573a2f8ebcb5da69f5e02
Dev Js <alienwaredev369@outlook.com>
2026-04-18 16:26:45 -0300

    Foi criada a rota de atualização com put

--- server.js
+++ server.js
@@ -25,5 +25,22 @@ app.post('/usuario', async (req, res) => {
     })
     res.status(201).json(user) //retornando usuário criado
 })
+//rota de atualização de usuário
+
+app.put('/usuario/:id', async (req, res) => {
+    //route params -> :id
+    const user = await prisma.user.update({
+        where: {
+            id: req.params.id
+
+        },
+        data: {
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email
+        }
+    })
+    res.status(201).json(user) //retornando usuário criado
+})
 
 app.listen(3000)
