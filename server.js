81e348ef90837125142c6af98fb208e917b24f81
Dev Js <alienwaredev369@outlook.com>
2026-04-18 15:11:20 -0300

    Foi incrementada a rota post com a contante prisma

--- server.js
+++ server.js
@@ -11,15 +11,21 @@ const prisma = new PrismaClient();
 //rota de listar, buscar e obter
 
 app.get('/usuario', (req, res) => {
-    
+
     res.status(200).json(users)
 })
 
 //rota de criar
 
-app.post('/usuario', (req, res) => {
-    users.push(req.body) //enviando a requisição para o arry users
-    res.status(201).json({message: "Usuário criado com sucesso"})
+app.post('/usuario', async (req, res) => {
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email
+        }
+    })
+    res.status(201).json({ message: "Usuário criado com sucesso" })
 })
 
 app.listen(3000)
